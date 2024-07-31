022e07a6b6e70ab31d16b00273b1282f99933857
Btt7601 <tintrung406@gmail.com>
2024-07-31 23:11:34 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace UserManager
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 199);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 350);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(12, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(200, 20);
+            this.txtName.TabIndex = 1;
+            this.txtName.Text = "Name";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(12, 77);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(200, 20);
+            this.txtAddress.TabIndex = 2;
+            this.txtAddress.Text = "Address";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(12, 144);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(200, 20);
+            this.txtPhone.TabIndex = 3;
+            this.txtPhone.Text = "Phone";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(598, 9);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(598, 47);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(598, 86);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(598, 129);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "User Management";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace UserManager
+{
+    public partial class Form1 : Form
+    {
+        private int currentId = 0;
+        private string connectionString = "Data Source=MSI\\SQLEXPRESS;Initial Catalog=CustomerDB;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddUser(txtName.Text, txtAddress.Text, txtPhone.Text);
+            LoadData();
+        }
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Users", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+        }
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            // Cập nhật các trường nhập liệu khi chọn một dòng
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                UpdateTextFieldsFromSelectedRow();
+            }
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Chỉ xử lý khi nhấp vào một ô hợp lệ (không phải header)
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                UpdateTextFieldsFromSelectedRow();
+            }
+        }
+
+        private void UpdateTextFieldsFromSelectedRow()
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow row = dataGridView1.SelectedRows[0];
+                txtName.Text = row.Cells["Name"].Value.ToString();
+                txtAddress.Text = row.Cells["Address"].Value.ToString();
+                txtPhone.Text = row.Cells["Phone"].Value.ToString();
+                currentId = Convert.ToInt32(row.Cells["Id"].Value);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (currentId != 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.Rows
+                    .Cast<DataGridViewRow>()
+                    .FirstOrDefault(row => Convert.ToInt32(row.Cells["Id"].Value) == currentId);
+
+                if (selectedRow != null)
+                {
+                    string currentName = selectedRow.Cells["Name"].Value.ToString();
+                    string currentAddress = selectedRow.Cells["Address"].Value.ToString();
+                    string currentPhone = selectedRow.Cells["Phone"].Value.ToString();
+
+                    string newName = string.IsNullOrEmpty(txtName.Text) ? currentName : txtName.Text;
+                    string newAddress = string.IsNullOrEmpty(txtAddress.Text) ? currentAddress : txtAddress.Text;
+                    string newPhone = string.IsNullOrEmpty(txtPhone.Text) ? currentPhone : txtPhone.Text;
+
+                    UpdateUser(currentId, newName, newAddress, newPhone);
+                    LoadData();
+                    ClearInputFields();
+                    currentId = 0;
+                }
+            }
+            else
+            {
+                MessageBox.Show("No user selected for update.");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["Id"].Value);
+                DeleteUser(id);
+                LoadData(); 
+                ClearInputFields(); 
+            }
+            else
+            {
+                MessageBox.Show("Please select a user to delete.");
+            }
+        }
+
+
+       
+
+
+        private void AddUser(string name, string address, string phone)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Users (Name, Address, Phone) VALUES (@Name, @Address, @Phone)", conn);
+                cmd.Parameters.AddWithValue("@Name", name);
+                cmd.Parameters.AddWithValue("@Address", address);
+                cmd.Parameters.AddWithValue("@Phone", phone);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void UpdateUser(int id, string name, string address, string phone)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE Users SET Name=@Name, Address=@Address, Phone=@Phone WHERE Id=@Id", conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+                cmd.Parameters.AddWithValue("@Name", name);
+                cmd.Parameters.AddWithValue("@Address", address);
+                cmd.Parameters.AddWithValue("@Phone", phone);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void DeleteUser(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("DELETE FROM Users WHERE Id=@Id", conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            return !string.IsNullOrWhiteSpace(txtName.Text) &&
+                   !string.IsNullOrWhiteSpace(txtAddress.Text) &&
+                   !string.IsNullOrWhiteSpace(txtPhone.Text) &&
+                   txtName.Text != "Name" &&
+                   txtAddress.Text != "Address" &&
+                   txtPhone.Text != "Phone";
+        }
+
+        private void ClearInputFields()
+        {
+            txtName.Text = "Name";
+            txtAddress.Text = "Address";
+            txtPhone.Text = "Phone";
+            txtName.ForeColor = System.Drawing.Color.Gray;
+            txtAddress.ForeColor = System.Drawing.Color.Gray;
+            txtPhone.ForeColor = System.Drawing.Color.Gray;
+        }
+    }
+}
+    
